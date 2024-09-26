63ac7c5d427c2067fa468a1f00ff19a8c5f9c3a7
Kumzha <kumza.dominykas@gmail.com>
2024-09-26 16:15:13 +0300

    First changes

--- fastapi/App/main.py
+++ fastapi/App/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 import schemas, crud
-from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from starlette.requests import Request
@@ -25,12 +24,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-templates = Jinja2Templates(directory="templates")
-
-@app.get('/main', response_class=HTMLResponse)
-def index(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
 
 @app.post('/submitAddress', response_model=list[schemas.Restaurant])
 def postaddress(payload: schemas.UserSelection):
